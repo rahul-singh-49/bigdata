e03cc95982ede6415ca10e0100211ca4b5bfc704
rahul-singh-49 <rahularathor222@gmail.com>
2026-09-02 00:42:06 +0530

    file update

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "subscription_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "tenant_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "client_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "client_secret" {
+  type      = string
+  sensitive = true
+}
