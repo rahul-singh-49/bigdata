8931beaafedd36e8cde90c6ef173834061071742
rahul-singh-49 <rahularathor222@gmail.com>
2026-09-02 01:02:16 +0530

    update yml

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-    subscription_id = var.subscription_id
+subscription_id= var.subscription_id
   tenant_id       = var.tenant_id
   client_id       = var.client_id
   client_secret   = var.client_secret
