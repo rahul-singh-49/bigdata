44b8dde10dc2eda0f274a2467f0c5132bce12ff1
rahul-singh-49 <rahularathor222@gmail.com>
2026-09-03 00:21:13 +0530

    update1

--- provider.tf
+++ provider.tf
@@ -9,9 +9,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-# subscription_id= var.subscription_id
-#   tenant_id       = var.tenant_id
-#   client_id       = var.client_id
-#   client_secret   = var.client_secret
-     skip_provider_registration = true
+
+    #  skip_provider_registration = true
 }
